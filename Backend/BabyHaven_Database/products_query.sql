75746db3ff7b44d444792a031e426bc777aa6b80
MuhammadGaf <muhammad.gaffar7@gmail.com>
2023-09-06 20:37:39 +0200

    query for all products
    i think yourll still have to run the sql on your laptops tho

---
+++ Backend/BabyHaven_Database/products_query.sql
@@ -0,0 +1,119 @@
+﻿INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('White Wooden Cot','This high quality cot is made of New Zealand Pine and comes with a mattress.',
+'Nursery Items',30,3500.99,'2023-09-06','BabyHaven\Frontend\img\shop\white-crib.jpeg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Solid Wood Baby Crib Cot','This high quality cot is made of New Zealand Pine and comes with a mattress.',
+'Nursery Items',25,2499.99,'2023-09-06','BabyHaven\Frontend\img\shop\wooden-crib.jpeg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Baby Monitor','A wireless two way baby monitor with speaker',
+'Nursery Items',100,1489.00,'2023-09-06','BabyHaven\Frontend\img\shop\baby-montior.jpeg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Multi Cradelchair','A multifunctional baby cradel chair',
+'Nursery Items',50,2399.99,'2023-09-06','BabyHaven\Frontend\img\shop\multi-cradlechair.jpeg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Wooden Cradlechair','A wooden bouncy baby cradle chair',
+'Nursery Items',75,1250.99,'2023-09-06','BabyHaven\Frontend\img\shop\wooden-cradlechair.jpeg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Convertible Car Seat','A LuvLap sports convertible car Seat',
+'Baby Gear',35,1199.99,'2023-09-06','BabyHaven\Frontend\img\shop\carseat.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Baby Stroller','An Infant comfortable baby stroller',
+'Baby Gear',100,850.00,'2023-09-06','BabyHaven\Frontend\img\shop\stroller.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Baby Swing','A cute and compact baby swing',
+'Baby Gear',100,950.00,'2023-09-06','BabyHaven\Frontend\img\shop\swing.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Playard','A safe Lullaby Baby Playard',
+'Baby Gear',30,6999.00,'2023-09-06','BabyHaven\Frontend\img\shop\playard.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Girls Onesie','A cute Girls onesie that has Daddys Girl printed on  it',
+'Baby Clothes',120,399.00,'2023-09-06','BabyHaven\Frontend\img\shop\onesie.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Cotton  Pyjamas','Pink and white 2pc Cotton Pyjamas',
+'Baby Clothes',80,199.00,'2023-09-06','BabyHaven\Frontend\img\shop\pjs.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Crochet Sweater','Cute crochet baby sweaters',
+'Baby Clothes',50,399.00,'2023-09-06','BabyHaven\Frontend\img\shop\sweater.jfif');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Baby Socks','5 pairs of assorted color baby socks',
+'Baby Clothes',70,419.99,'2023-09-06','BabyHaven\Frontend\img\shop\socks.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('NUK bottle','High Quality NUK baby bottles',
+'Feeding Essentials',100,199.99,'2023-09-06','BabyHaven\Frontend\img\shop\bottle.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Skattig Bib','Premium Quality Skattig branded bibs',
+'Feeding Essentials',100,80.00,'2023-09-06','BabyHaven\Frontend\img\shop\bib.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Pacifier','Baby pink pacifiers',
+'Feeding Essentials',80,100.00,'2023-09-06','BabyHaven\Frontend\img\shop\dummy.jfif');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Digital Themometer','Digital Themometer',
+'Health Products',50,59.99,'2023-09-06','BabyHaven\Frontend\img\shop\thermo.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Healthcare Kit','1st aid safety healthcare kit',
+'Health Products',50,289.99,'2023-09-06','BabyHaven\Frontend\img\shop\firstaid1.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Waterproof Mattress','Soft and comfortable Newton waterproof mattress for babies',
+'Baby Bedding',50,1520.00,'2023-09-06','BabyHaven\Frontend\img\shop\matresspad1.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Snuggletime Fitted Sheet','Snuggletime stay safe fitted sheet',
+'Baby Bedding',50,99.99,'2023-09-06','BabyHaven\Frontend\img\shop\babysheets.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Reciever Blanket','Soft and warm BabySense Reciever blanket',
+'Baby Bedding',50,189.00,'2023-09-06','BabyHaven\Frontend\img\shop\blanket.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Chelino Prima Rossi Change Table','High qualtiy and beautiful Chelino Prima Rossi Change Table',
+'Diapering Must-Haves',30,2299.00,'2023-09-06','BabyHaven\Frontend\img\shop\babytable.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Ryco Willow Diaper Bag','High qualtiy Ryco Willow Diaper Bag',
+'Diapering Must-Haves',30,1099.99,'2023-09-06','BabyHaven\Frontend\img\shop\diaperbag.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Johnsons Baby Diaper Cream','Johnsons Baby Diaper Cream',
+'Diapering Must-Haves',30,199.99,'2023-09-06','BabyHaven\Frontend\img\shop\diaperceream.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Pampers Premium Care Diapers','Pampers Premium Care Diapers',
+'Diapering Must-Haves',30,269.99,'2023-09-06','BabyHaven\Frontend\img\shop\diapers.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Chellino Bath Tub','Chellino Bath Tub for easy use',
+'Bath Items',60,599.99,'2023-09-06','BabyHaven\Frontend\img\shop\bathtub.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Snuggle Time Soft Hooded Towel',' White Snuggle Time Soft Hooded Towel',
+'Bath Items',60,84.99,'2023-09-06','BabyHaven\Frontend\img\shop\towel.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Safety First Easy Brush & Comb','Plastic Safety First Easy  Baby Brush & Comb',
+'Bath Items',60,94.99,'2023-09-06','BabyHaven\Frontend\img\shop\brush.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('Snuggletime Washcloth','Soft Snuggletime Washcloth',
+'Bath Items',60,99.99,'2023-09-06','BabyHaven\Frontend\img\shop\brush.jpg');
+
+INSERT INTO Product(P_Name,P_Description,P_Category,P_Quantity,P_Price,P_DateCreated,P_Image)
+VALUES('SafeHaven Project Sock','SafeHaven Project Sock(sock to be purchased for saferty of mothers)',
+'Project SafeHaven',1000,0.00,'2023-09-06','BabyHaven\Frontend\img\shop\safehaven.jpg');
