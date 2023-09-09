551881c7c4a7d2748905aba3f939aebed469e869
NasiraSeedat <seedatnasira@gmail.com>
2023-09-09 13:08:14 +0200

    changed blogdetails

--- Frontend/BabyHaven.Master.designer.cs
+++ Frontend/BabyHaven.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Frontend
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl adminLink;
 
         /// <summary>
+        /// DropAdmin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl DropAdmin;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
