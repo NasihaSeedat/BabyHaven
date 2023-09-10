2fbd1b041c7d9e2d7a4f5ee211de44adfe979f60
MuhammadGaf <muhammad.gaffar7@gmail.com>
2023-09-10 15:14:32 +0200

    load items acc to category(final)

--- Frontend/Shop.aspx.cs
+++ Frontend/Shop.aspx.cs
@@ -14,49 +14,109 @@ namespace Frontend
         protected void Page_Load(object sender, EventArgs e)
         {
             
-            string display = " ";
-            int n = 0;
+            
             if (Request.QueryString["Category"] == null)
             {
                 dynamic prods = client.Getallproducts();
-                display += "<div class='col-lg-9 col-md-9'><div class='row'>";
-
-                foreach (BackendReference.Product pr in prods)
-                {
-                    display += "<div class='col-lg-4 col-md-6'>";
-                    display += "<div class='product__item'>" + "<div class='product__item__pic set-bg' data-setbg='" + pr.P_Image + "'>";
-                    display += "<ul class='product__hover'>" +
-                    "<li><a href='" + pr.P_Image + "' class='image-popup'><span class='arrow_expand'></span></a></li>" +
-                    "<li><a href='#'><span class='fa fa-shopping-cart'></span></a></li>" +
-                    "</ul>" +
-                    "</div>";
-                    display += "<div class='product__item__text'>";
-                    display += "<h6><a href='#'>" + pr.P_Name + "</a></h6>" + "<br />";
-                    display += "<div class='product__price'>" + String.Format("{0:0.00}", pr.P_Price);
-                    display += "</div></div></div></div>";
-
-                    n++;
-
-                    if (n % 3 == 0)
-                    {
-                        display += "</div><div class='row'>";
-                    }
-                }
-                if (n % 3 != 0)
-                {
-                    display += "</div>";
-                }
-
-                display += "</div>";
-                productDisplay.Text = display;
+                displaymethod(prods);
             }else if (Request.QueryString["Category"] != null)
             {
                 string cat = Request.QueryString["Category"];
-                if(cat.Equals("Nursery Items"))
+                dynamic prods;
+                if (cat.Equals("Nursery Items"))
                 {
-                    dynamic prods = client.getProductCat("Nursery Items");
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Nursery Items");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Baby Gear"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Baby Gear");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Baby Clothes"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Baby Clothes");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Feeding Essentials"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Feeding Essentials");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Health Products"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Health Products");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Baby Bedding"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Baby Bedding");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Diapering Must-Haves"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Diapering Must-Haves");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Bath Items"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Bath Items");
+                    displaymethod(prods);
+                }
+                else if (cat.Equals("Project SafeHaven"))
+                {
+                    //create variable with prodcuts from specific catoegory 
+                    prods = client.getProductCat("Project SafeHaven");
+                    displaymethod(prods);
+                }
+
+                }
+        }
+        private void displaymethod(dynamic prods)
+        {
+            string display = " ";
+            int n = 0;
+
+            display += "<div class='col-lg-9 col-md-9'><div class='row'>";
+
+            foreach (BackendReference.Product pr in prods)
+            {
+
+                display += "<div class='col-lg-4 col-md-6'>";
+                display += "<div class='product__item'>" + "<div class='product__item__pic set-bg' data-setbg='" + pr.P_Image + "'>";
+                display += "<ul class='product__hover'>" +
+                "<li><a href='" + pr.P_Image + "' class='image-popup'><span class='arrow_expand'></span></a></li>" +
+                "<li><a href='#'><span class='fa fa-shopping-cart'></span></a></li>" +
+                "</ul>" +
+                "</div>";
+                display += "<div class='product__item__text'>";
+                display += "<h6><a href='#'>" + pr.P_Name + "</a></h6>" + "<br />";
+                display += "<div class='product__price'>" + String.Format("{0:0.00}", pr.P_Price);
+                display += "</div></div></div></div>";
+
+                n++;
+
+                if (n % 3 == 0)
+                {
+                    display += "</div><div class='row'>";
                 }
             }
+            if (n % 3 != 0)
+            {
+                display += "</div>";
+            }
+
+            display += "</div>";
+            productDisplay.Text = display;
         }
     }
+    
 }
